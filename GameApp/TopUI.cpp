ac9ce913e2576cfb731ce86986cc47a748c860b0
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2022-07-14 19:43:55 +0900

    이건호 TopUI 테스트용 이미지 커밋

--- GameApp/TopUI.cpp
+++ GameApp/TopUI.cpp
@@ -23,13 +23,17 @@ void TopUI::Start()
 {
 	
 	TopRenderer = CreateTransformComponent<GameEngineUIRenderer>(GetTransform());
-	TopRenderer->SetImage("HPBarBackground_OutLine.png", "PointSmp");
+	TopRenderer->SetImage("AlphaTest.png", "PointSmp");
 	TopRenderer->GetTransform()->SetLocalPosition({ 0.0f, 0.0f, 0.0f });
 	TopRenderer->GetTransform()->SetLocalScaling(TopRenderer->GetCurrentTexture()->GetTextureSize());
 	//TopRenderer->SetAlpha(0.0f);
 	//TopRenderer->TextSetting("궁서", "테스트 중입니다", 50);
+
+	//알파값이 제대로 들어오는중, 이미지 문제는 아니다
+	float4 color = TopRenderer->GetCurrentTexture()->GetPixel(30,150);
+	float4 color2 = TopRenderer->GetCurrentTexture()->GetPixel(50, 100);
 	
-	UIRendererMap.insert(make_pair("HPBarBackground_OutLine", TopRenderer));
+	UIRendererMap.insert(make_pair("AlphaTest", TopRenderer));
 
 }
 
