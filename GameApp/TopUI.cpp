6acae897a8c152f7345139a44f54f439215e990e
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2022-07-15 13:25:27 +0900

    이건호 임시 체력바 UI 출력

--- GameApp/TopUI.cpp
+++ GameApp/TopUI.cpp
@@ -1,9 +1,10 @@
 #include "PreCompile.h"
 #include "TopUI.h"
 #include "GameEngine/GameEngineUIRenderer.h"
+#include "GameEngine/GameEngineInput.h"
 
 TopUI::TopUI()
-	: Time(3.0f)
+	: Time(1.0f)
 {
 }
 
@@ -21,26 +22,60 @@ TopUI::~TopUI()
 
 void TopUI::Start()
 {
-	
-	TopRenderer = CreateTransformComponent<GameEngineUIRenderer>(GetTransform());
-	TopRenderer->SetImage("AlphaTest.png", "PointSmp");
-	TopRenderer->GetTransform()->SetLocalPosition({ 0.0f, 0.0f, 0.0f });
-	TopRenderer->GetTransform()->SetLocalScaling(TopRenderer->GetCurrentTexture()->GetTextureSize());
-	//TopRenderer->SetAlpha(0.0f);
-	//TopRenderer->TextSetting("궁서", "테스트 중입니다", 50);
+	//리소스들을 SetImage하고 위치를 조정합니다
+	//위치를 조정한 리소스들은 UIRenderMap에 String과 같이 insert됩니다.
+	//인게임정보(HP나 스태미너 상태, 착용장비여부)등을 받으면, UIRendererMap에서 이미지이름으로 Find해서 리소스를 바꿀 예정업니다.
+	{
+		//z값을 이용해 앞에오는 이미지/뒤에오는 이미지 순서를 정하고 있습니다.
+		//위치정보가 될 float도 양이 늘어나면 map이나 vector로 관리할 예정입니다.
+		float4 BackGroundBarPos = { -400.0f, -200.0f, 0.0f };
+		float4 HPBarPos = { -395.0f, -202.0f, -1.0f };
+		float4 HPBarTopPos = { -394.0f, -143.0f, -1.0f };
+		float4 HPBarLinePos = { -401.0f, -180.0f, -2.0f};
 
-	//알파값이 제대로 들어오는중, 이미지 문제는 아니다
-	float4 color = TopRenderer->GetCurrentTexture()->GetPixel(30,150);
-	float4 color2 = TopRenderer->GetCurrentTexture()->GetPixel(50, 100);
-	
-	UIRendererMap.insert(make_pair("AlphaTest", TopRenderer));
+		{
+			TopRenderer = CreateTransformComponent<GameEngineUIRenderer>(GetTransform());
+			TopRenderer->SetImage("BackGroundBar_Test.png", "PointSmp");
+			TopRenderer->GetTransform()->SetLocalPosition(BackGroundBarPos);
+			TopRenderer->GetTransform()->SetLocalScaling(TopRenderer->GetCurrentTexture()->GetTextureSize());
+			UIRendererMap.insert(make_pair("BackGroundBar_Test", TopRenderer));
+		}
+
+		{
+			TopRenderer = CreateTransformComponent<GameEngineUIRenderer>(GetTransform());
+			TopRenderer->SetImage("HPBar_example.png", "PointSmp");
+			TopRenderer->GetTransform()->SetLocalPosition(HPBarPos);
+			TopRenderer->GetTransform()->SetLocalScaling(TopRenderer->GetCurrentTexture()->GetTextureSize());
+			UIRendererMap.insert(make_pair("HPBar_example", TopRenderer));
+		}
+
+		{
+			TopRenderer = CreateTransformComponent<GameEngineUIRenderer>(GetTransform());
+			TopRenderer->SetImage("HPBar_Top.png", "PointSmp");
+			TopRenderer->GetTransform()->SetLocalPosition(HPBarTopPos);
+			TopRenderer->GetTransform()->SetLocalScaling(TopRenderer->GetCurrentTexture()->GetTextureSize());
+			UIRendererMap.insert(make_pair("HPBar_Top", TopRenderer));
+		}
+
+		{
+			TopRenderer = CreateTransformComponent<GameEngineUIRenderer>(GetTransform());
+			TopRenderer->SetImage("Bar_Line.png", "PointSmp");
+			TopRenderer->GetTransform()->SetLocalPosition(HPBarLinePos);
+			TopRenderer->GetTransform()->SetLocalScaling(TopRenderer->GetCurrentTexture()->GetTextureSize());
+			UIRendererMap.insert(make_pair("Bar_Line", TopRenderer));
+		}
+	}
 
 }
 
 void TopUI::Update(float _Time)
 {
-	//if (0.0f >= (Time -= _Time))
-	//{
-	//	TopRenderer->Death();
-	//}
+	if (0.0f >= (Time -= _Time))
+	{
+		//TopRenderer->SetAlpha(Time);
+	}
+
+
+//	TopRenderer->SetAlpha(0.1f);
 }
+
