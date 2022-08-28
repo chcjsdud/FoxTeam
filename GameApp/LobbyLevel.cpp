c63cd4ed9984649db336b0696bdd5a926f4d6356
Won Park <nerichagi21@gmail.com>
2022-08-29 02:44:02 +0900

    박종원 : 로비레벨 커밋

--- GameApp/LobbyLevel.cpp
+++ GameApp/LobbyLevel.cpp
@@ -1,2 +1,32 @@
 #include "Precompile.h"
 #include "LobbyLevel.h"
+
+LobbyLevel::LobbyLevel()
+{
+
+}
+
+LobbyLevel::~LobbyLevel()
+{
+
+}
+
+void LobbyLevel::LevelStart()
+{
+
+}
+
+void LobbyLevel::LevelUpdate(float _DeltaTime)
+{
+
+}
+
+void LobbyLevel::LevelChangeEndEvent(GameEngineLevel* _NextLevel)
+{
+
+}
+
+void LobbyLevel::LevelChangeStartEvent(GameEngineLevel* _PrevLevel)
+{
+
+}
--- GameApp/LobbyLevel.h
+++ GameApp/LobbyLevel.h
@@ -1,5 +1,41 @@
 #pragma once
-class LobbyLevel
+#include <GameEngine/GameEngineLevel.h>
+#include <GameEngine/GameEngineFSM.h>
+
+#include <GameEngine/GameEngineSocketServer.h>
+#include <GameEngine/GameEngineSocketClient.h>
+
+class LobbyLevel : public GameEngineLevel
 {
+public:
+	LobbyLevel(); 
+	~LobbyLevel();
+	LobbyLevel(const LobbyLevel& _other) = delete;
+	LobbyLevel(LobbyLevel&& _other) noexcept = delete;
+	LobbyLevel& operator=(const LobbyLevel& _other) = delete;
+	LobbyLevel& operator=(const LobbyLevel&& _other) = delete;
+
+public:
+	// 스테이트를 나눠 사용하는 게 옳을 것
+	// Idle :: 아무 입력이 들어가지 않은 초기 스테이트
+	// CharSelect :: 캐릭터 셀렉트와 시작 지점 설정을 위한 스테이트?
+	// (플레이어 정보... 캐릭터 선택 시작 위치 등을 담을 수 있는 패킷을 준비해야 한다.)
+	// LoginPacket? CharSelectPacket? 이름을 무엇으로?
+
+	
+	virtual void LevelStart() override;
+	virtual void LevelUpdate(float _DeltaTime) override;
+	virtual void LevelChangeEndEvent(GameEngineLevel* _NextLevel) override;
+	virtual void LevelChangeStartEvent(GameEngineLevel* _PrevLevel) override;
+
+protected:
+
+
+private:
+	GameEngineFSM* state_;
+
+	GameEngineSocketServer serverSocket_;
+	GameEngineSocketClient clientSocket_;
+
 };
 
