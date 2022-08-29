cc6049feeb3d34313b868038c60e9223a6f4a08e
Ratisser <w_vista@naver.com>
2022-08-29 21:59:45 +0900

    필터 업데이트

--- GameApp/LumiaLevel.cpp
+++ GameApp/LumiaLevel.cpp
@@ -1,2 +1,26 @@
 #include "Precompile.h"
 #include "LumiaLevel.h"
+
+LumiaLevel::LumiaLevel()
+{
+}
+
+LumiaLevel::~LumiaLevel()
+{
+}
+
+void LumiaLevel::LevelStart()
+{
+}
+
+void LumiaLevel::LevelUpdate(float _DeltaTime)
+{
+}
+
+void LumiaLevel::LevelChangeEndEvent(GameEngineLevel* _NextLevel)
+{
+}
+
+void LumiaLevel::LevelChangeStartEvent(GameEngineLevel* _PrevLevel)
+{
+}
--- GameApp/LumiaLevel.h
+++ GameApp/LumiaLevel.h
@@ -1,5 +1,19 @@
 #pragma once
-class LumiaLevel
+
+#include <GameEngine/GameEngineLevel.h>
+
+class LumiaLevel : public GameEngineLevel
 {
+public:
+	LumiaLevel();
+	~LumiaLevel();
+
+public:
+	virtual void LevelStart() override;
+	virtual void LevelUpdate(float _DeltaTime) override;
+	virtual void LevelChangeEndEvent(GameEngineLevel* _NextLevel) override;
+	virtual void LevelChangeStartEvent(GameEngineLevel* _PrevLevel) override;
+
+
 };
 
