4a8e57cd3df5246c6082e2476fcb79f3b90f2fd9
Ratisser <w_vista@naver.com>
2022-10-10 13:28:47 +0900

    Add SendPacket global function

--- GameApp/Macro.h
+++ GameApp/Macro.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define CINT(type) static_cast<int>(type)
+#include "GameServer.h"
+#include "GameClient.h"
 
 #define MakeState(ClassName, StateName) #StateName, std::bind(&ClassName::update##StateName, this, std::placeholders::_1) \
 		, std::bind(&ClassName::start##StateName, this)
@@ -8,3 +9,16 @@
 #define MakeStateWithEnd(ClassName, StateName) #StateName, std::bind(&ClassName::update##StateName, this, std::placeholders::_1) \
 		, std::bind(&ClassName::start##StateName, this) \
 		, std::bind(&ClassName::end##StateName, this)
+
+
+void SendPacket(GameEnginePacketBase& packet)
+{
+	if (true == GameServer::GetInstance()->IsOpened())
+	{
+		GameServer::GetInstance()->Send(&packet);
+	}
+	else if (true == GameClient::GetInstance()->IsConnected())
+	{
+		GameClient::GetInstance()->Send(&packet);
+	}
+}
