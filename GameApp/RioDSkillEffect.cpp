a1876a1b85dd388b2f657ed46c5d323696427776
Won Park <nerichagi21@gmail.com>
2022-11-09 14:35:55 +0900

    박종원 : 리오 D 이펙트 커밋

---
+++ GameApp/RioDSkillEffect.cpp
@@ -0,0 +1,119 @@
+#include "PreCompile.h"
+#include "RioDSkillEffect.h"
+#include <GameEngine/GameEngineEffectRenderer.h>
+
+RioDSkillEffect::RioDSkillEffect() // default constructer 디폴트 생성자
+	: zoneRenderer_(nullptr), impactRenderer_(nullptr), collision_(nullptr), timer_(0.0f), rotationContainer_(0.0f), impactOriginalScale_({0.0f,0.0f})
+	, impactScaleContainer_({0.0f,0.0f}), shotTimer_(0.3f)
+{
+
+}
+
+RioDSkillEffect::~RioDSkillEffect() // default destructer 디폴트 소멸자
+{
+
+}
+
+RioDSkillEffect::RioDSkillEffect(RioDSkillEffect&& _other) noexcept  // default RValue Copy constructer 디폴트 RValue 복사생성자
+{
+
+}
+
+void RioDSkillEffect::PlayAwake()
+{
+	renderState_ << "Awake";
+}
+
+void RioDSkillEffect::PlayShot()
+{
+}
+
+void RioDSkillEffect::Start()
+{
+	zoneRenderer_ = CreateTransformComponent<GameEngineEffectRenderer>();
+	zoneRenderer_->SetImage("wMask.png", "LinerSmp");
+	zoneRenderer_->GetTransform()->SetLocalPosition({ 0.0f, 20.0f, 0.0f });
+	zoneRenderer_->GetTransform()->SetLocalRotationDegree({ 90.f,0.f,0.f });
+	zoneRenderer_->GetTransform()->SetLocalScaling(zoneRenderer_->GetCurrentTexture()->GetTextureSize() * 3.0f);
+	zoneRenderer_->SetAlpha(0.0f);
+	zoneRenderer_->Off();
+
+	impactRenderer_ = CreateTransformComponent<GameEngineEffectRenderer>();
+	impactRenderer_->SetImage("rio_dskillround.png", "LinerSmp");
+	impactRenderer_->GetTransform()->SetLocalPosition({ 0.0f, 25.0f, 0.0f });
+	impactRenderer_->GetTransform()->SetLocalRotationDegree({ 90.f, 0.f,0.f });
+	impactRenderer_->GetTransform()->SetLocalScaling(impactRenderer_->GetCurrentTexture()->GetTextureSize() * 2.0f);
+	impactOriginalScale_ = impactRenderer_->GetTransform()->GetLocalScaling();
+	impactRenderer_->SetAlpha(0.0f);
+	impactRenderer_->Off();
+
+	targetRenderer_ = CreateTransformComponent<GameEngineEffectRenderer>();
+	targetRenderer_->SetImage("rio_dskillround.png", "LinerSmp");
+	targetRenderer_->GetTransform()->SetLocalPosition({ 0.0f, 25.0f, 0.0f });
+	targetRenderer_->GetTransform()->SetLocalRotationDegree({ 90.f, 0.f,0.f });
+	targetRenderer_->GetTransform()->SetLocalScaling(impactRenderer_->GetCurrentTexture()->GetTextureSize() * 2.0f);
+	impactOriginalScale_ = impactRenderer_->GetTransform()->GetLocalScaling();
+	targetRenderer_->SetAlpha(0.0f);
+	targetRenderer_->Off();
+
+	//collision_ = CreateTransformComponent<GameEngineCollision>(GetTransform());
+	//collision_->GetTransform()->SetLocalPosition({ 0.f,0.f,0.f });
+	//collision_->GetTransform()->SetLocalScaling({ 450.0f, 450.0f, 450.0f });
+	//collision_->SetCollisionGroup(eCollisionGroup::PlayerAttack);
+	//collision_->SetCollisionType(CollisionType::CirCle);
+	//collision_->Off();
+
+	renderState_.CreateState(MakeState(RioDSkillEffect, Sleep));
+	renderState_.CreateState(MakeState(RioDSkillEffect, Awake));
+	renderState_.CreateState(MakeState(RioDSkillEffect, Shot));
+	renderState_ << "Sleep";
+}
+
+void RioDSkillEffect::Update(float _deltaTime)
+{
+	renderState_.Update(_deltaTime);
+}
+
+void RioDSkillEffect::startSleep()
+{
+	impactRenderer_->Off();
+	zoneRenderer_->Off();
+	zoneRenderer_->SetAlpha(0.0f);
+	impactRenderer_->SetAlpha(0.0f);
+
+}
+
+void RioDSkillEffect::updateSleep(float _deltaTime)
+{
+}
+
+void RioDSkillEffect::startAwake()
+{
+	impactRenderer_->On();
+	zoneRenderer_->On();
+}
+
+void RioDSkillEffect::updateAwake(float _deltaTime)
+{
+	timer_ += _deltaTime;
+
+	if (timer_ <= 1.0f)
+	{
+		impactRenderer_->SetAlpha(timer_);
+		zoneRenderer_->SetAlpha(timer_);
+	}
+	
+	impactScaleContainer_ += {120.0f * _deltaTime, 120.0f * _deltaTime};
+	impactRenderer_->GetTransform()->SetLocalScaling(impactOriginalScale_ + impactScaleContainer_);
+}
+
+void RioDSkillEffect::startShot()
+{
+	shotTimer_ = 0.3f;
+}
+
+void RioDSkillEffect::updateShot(float _deltaTime)
+{
+
+}
+
---
+++ GameApp/RioDSkillEffect.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <GameEngine/GameEngineActor.h>
+#include <GameEngine/GameEngineFSM.h>
+#include "IUnit.h"
+
+class GameEngineEffectRenderer;
+class RioDSkillEffect : public GameEngineActor
+{
+public:
+	RioDSkillEffect(); // default constructer 디폴트 생성자
+	~RioDSkillEffect(); // default destructer 디폴트 소멸자
+	RioDSkillEffect(const RioDSkillEffect& _other) = delete; // default Copy constructer 디폴트 복사생성자
+	RioDSkillEffect(RioDSkillEffect&& _other) noexcept; // default RValue Copy constructer 디폴트 RValue 복사생성자
+	RioDSkillEffect& operator=(const RioDSkillEffect& _other) = delete; // default Copy operator 디폴트 대입 연산자
+	RioDSkillEffect& operator=(const RioDSkillEffect&& _other) = delete; // default RValue Copy operator 디폴트 RValue 대입연산자
+
+public:
+	void PlayAwake();
+	void PlayShot();
+
+public:
+	virtual void Start() override;
+	virtual void Update(float _deltaTime);
+
+protected:
+	void startSleep();
+	void updateSleep(float _deltaTime);
+
+	void startAwake();
+	void updateAwake(float _deltaTime);
+
+	void startShot();
+	void updateShot(float _deltaTime);
+
+private:
+	GameEngineFSM renderState_;
+	GameEngineEffectRenderer* zoneRenderer_;
+	GameEngineEffectRenderer* impactRenderer_;
+	GameEngineEffectRenderer* targetRenderer_;
+	GameEngineCollision* collision_;
+
+private:
+	float timer_;
+	float rotationContainer_;
+
+	float4 impactScaleContainer_;
+	float4 impactOriginalScale_;
+
+	float shotTimer_;
+};
+
