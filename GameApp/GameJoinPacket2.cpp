9c29ee915b68c5ae2c38d4a3774cca19945a5ea5
Won Park <nerichagi21@gmail.com>
2022-11-17 14:47:28 +0900

    박종원 : 게임조인패킷2 예외처리 수정

--- GameApp/GameJoinPacket2.cpp
+++ GameApp/GameJoinPacket2.cpp
@@ -59,50 +59,50 @@ void GameJoinPacket2::execute(SOCKET _sender, GameEngineSocketInterface* _networ
 {
     PlayerInfoManager* pm = PlayerInfoManager::GetInstance();
 
-    if ("LumiaLevel" == GameEngineCore::CurrentLevel()->GetName())
+    if ("LobbyLevel" == GameEngineCore::CurrentLevel()->GetName())
     {
-        return;
-    }
-
-    if (_bServer)
-    {
-        // 클라이언트가 보낸 리스트 사이즈가 0 이면 최초 접속으로 간주한다.
-        if (listSize_ == 0)
+        if (_bServer)
         {
-            for (size_t i = 0; i < pm->GetPlayerList().size(); i++)
+            // 클라이언트가 보낸 리스트 사이즈가 0 이면 최초 접속으로 간주한다.
+            if (listSize_ == 0)
             {
-                GameJoinPacket2 p;
-                p.SetPlayerInfo(pm->GetPlayerList()[i]);
-                _network->Send(_sender, &p);
+                for (size_t i = 0; i < pm->GetPlayerList().size(); i++)
+                {
+                    GameJoinPacket2 p;
+                    p.SetPlayerInfo(pm->GetPlayerList()[i]);
+                    _network->Send(_sender, &p);
+                }
             }
+            else if (listSize_ < 0)
+            {
+                GameEngineDebug::OutPutDebugString("잘못된 리스트 사이즈를 전송받았습니다.");
+                return;
+            }
+
+            // 이건 접속한 클라이언트의 정보가 될 것이다.
+            info_.playerNumber_ = static_cast<int>(pm->GetPlayerList().size());
+            info_.character_ = -1;
+            info_.startPoint_ = -1;
+            info_.isReady_ = 0;
+
+
+            // 새로운 클라이언트 정보를 서버와 클라이언트들에게 추가한다.
+            pm->AddNewPlayer(info_);
+            serializer_.OffsetReset();
+            _network->Send(this);
+
+            // 접속한 클라이언트 본인에게는 플레이어리스트의 인덱스를 부여한다.
+            SetPlayerNumberPacket packet;
+            packet.SetNumber(info_.playerNumber_);
+            _network->Send(_sender, &packet);
+
         }
-        else if (listSize_ < 0)
+        else // client
         {
-            GameEngineDebug::OutPutDebugString("잘못된 리스트 사이즈를 전송받았습니다.");
-            return;
+            pm->AddNewPlayer(info_);
         }
-
-        // 이건 접속한 클라이언트의 정보가 될 것이다.
-        info_.playerNumber_ = static_cast<int>(pm->GetPlayerList().size());
-        info_.character_ = -1;
-        info_.startPoint_ = -1;
-        info_.isReady_ = 0;
-        
-
-        // 새로운 클라이언트 정보를 서버와 클라이언트들에게 추가한다.
-        pm->AddNewPlayer(info_);
-        serializer_.OffsetReset();
-        _network->Send(this);
-
-        // 접속한 클라이언트 본인에게는 플레이어리스트의 인덱스를 부여한다.
-        SetPlayerNumberPacket packet;
-        packet.SetNumber(info_.playerNumber_);
-        _network->Send(_sender, &packet);
-        
     }
-    else // client
-    {
-        pm->AddNewPlayer(info_);
-    }
+
+   
 }
 
