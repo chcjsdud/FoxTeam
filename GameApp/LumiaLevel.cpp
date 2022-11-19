943fc093b438f54f4b6fdf1b6f622eccf1a46e38
Ratisser <w_vista@naver.com>
2022-11-19 18:44:56 +0900

    Update LumiaLevel.cpp

--- GameApp/LumiaLevel.cpp
+++ GameApp/LumiaLevel.cpp
@@ -744,7 +744,7 @@ void LumiaLevel::InitIMGUIWindow()
 		ItemListWindow_->ItemBoxManager_ = ItemBoxManager_;
 	}
 
-	//ItemListWindow_->Off();
+	ItemListWindow_->Off();
 
 	if (nullptr != Window && nullptr != ItemBoxManager_)
 	{
